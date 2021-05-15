b3d26c2681f1734a060a99780ae85e19fa1946b2
jasonlara <jasonlara@10.211.55.3>
2021-05-15 10:51:55 -0600

    changesUpdate

--- Examen01_Prog2_Caleb_Castaneda_Jason_Mendez/src/modelo/Persona.java
+++ Examen01_Prog2_Caleb_Castaneda_Jason_Mendez/src/modelo/Persona.java
@@ -2,4 +2,27 @@ package modelo;
 
 public class Persona {
 
+	String nombre;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEdad() {
+		return edad;
+	}
+	public void setEdad(String edad) {
+		this.edad = edad;
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	String edad;
+	String cedula;
+	
+	
 }
